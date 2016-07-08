4ae19806a550865e21f7bdc4cc69938d4b9064ea
WizTonE <WizTonE@WIZTONE-PC>
2016-07-08 13:20:46 +0800

    1. 修改Prodoction Code:     [Class] OrderData --> OrderProcess     [Class] 移除generic宣告     [Property] IEnumerable<T> dataSource 移除, 改由Function GetDataByPaging<T> 傳入     [Function] GetDataByGroup --> GetDataByPaging<T>     [Function] GetDataByPaging<T>(decimal groupNumber) --> GetDataByPaging<T>(int groupNumber) 2. 修改測試專案     [Class] OrderDataTests --> OrderPagingTests     [Class] OrderData --> Order     [Property] IEnumerable<Order> 加上private宣告, 並由orderdata 改名為 orders     [Function] InitialOrderData --> InitialOrders     [Function] InitialOrders  [AssemblyInitialize] --> [TestInitialize]     [TestMethod]  配合OrderProcess做修正,  移除datasource設定, 將orders直接傳入Function GetDataByPaging

--- TDD_Homework1/OrderProcess.cs
+++ TDD_Homework1/OrderProcess.cs
@@ -6,21 +6,14 @@ using System.Threading.Tasks;
 
 namespace TDD_Homework1
 {
-    public class OrderData<T>
+    public class OrderProcess
     {
-        IEnumerable<T> dataSource { get; set; }
-
-        public OrderData()
+        public OrderProcess()
         {
 
         }
 
-        public void SetDatasource(IEnumerable<T> _dataSource)
-        {
-            dataSource = _dataSource;
-        }
-
-        public List<int> GetDataByGroup(decimal groupNumber, Func<T, int> selectProperty)
+        public List<int> GetDataByPaging<T>(IEnumerable<T> dataSource, int groupNumber, Func<T, int> selectProperty) where T : class, new()
         {
             var groupList = new List<int>();
 
@@ -30,7 +23,7 @@ namespace TDD_Homework1
 
                 groupList = subList
                     .Select((x, i) => new { index = i + 1, value = x })//針對每一筆建立索引
-                    .GroupBy(g => Math.Ceiling(g.index / groupNumber))//針對索引組合群組
+                    .GroupBy(g => Math.Ceiling(g.index / (decimal)groupNumber))//針對索引組合群組
                     .Select(grp => grp.Select(v => v.value).ToList().Sum())//針對群組做Sum
                     .ToList();
             }
---
+++ TDD_Homework1Tests/OrderPagingTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_Homework1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpectedObjects;
+
+namespace TDD_Homework1.Tests
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public int COST { get; set; }
+        public int REVENUE { get; set; }
+        public int SELLPRICE { get; set; }
+    }
+
+    [TestClass()]
+    public class OrderPagingTests
+    {
+        private IEnumerable<Order> orders { get; set; }
+
+        //測試專案初始化
+        [TestInitialize]
+        public void InitialOrders()
+        {
+            orders = new List<Order>
+            {
+                new Order() { ID=1,  COST=1,  REVENUE=11,  SELLPRICE=21 },
+                new Order() { ID=2,  COST=2,  REVENUE=12,  SELLPRICE=22 },
+                new Order() { ID=3,  COST=3,  REVENUE=13,  SELLPRICE=23 },
+                new Order() { ID=4,  COST=4,  REVENUE=14,  SELLPRICE=24 },
+                new Order() { ID=5,  COST=5,  REVENUE=15,  SELLPRICE=25 },
+                new Order() { ID=6,  COST=6,  REVENUE=16,  SELLPRICE=26 },
+                new Order() { ID=7,  COST=7,  REVENUE=17,  SELLPRICE=27 },
+                new Order() { ID=8,  COST=8,  REVENUE=18,  SELLPRICE=28 },
+                new Order() { ID=9,  COST=9,  REVENUE=19,  SELLPRICE=29 },
+                new Order() { ID=10, COST=10, REVENUE=20,  SELLPRICE=30 },
+                new Order() { ID=11, COST=11, REVENUE=21,  SELLPRICE=31 },
+            };
+        }
+
+        [TestMethod()]
+        public void Get_Cost_Property_expected_List_6_15_24_21()
+        {
+            //Arrange
+            var expected = new List<int> { 6, 15, 24, 21 };
+            var service = new OrderProcess();
+
+            //Act
+            Func<Order, int> selectProperty = s => s.COST;
+            var actual = service.GetDataByPaging(orders, 3, selectProperty);
+            
+            //Assert
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod()]
+        public void Get_Revenue_Property_expected_List_50_66_60()
+        {
+            //Arrange
+            var expected = new List<int> { 50, 66, 60 };
+            var service = new OrderProcess();
+            
+            //Act
+            Func<Order, int> selectProperty = s => s.REVENUE;
+            var actual = service.GetDataByPaging(orders, 4, selectProperty);
+            
+            //Assert
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+}
